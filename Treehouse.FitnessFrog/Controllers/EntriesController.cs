b9bb1f3c56ec05884cb488b9ee0f0d41b44e1e91
Cristian Lazurean <cristian_lazurean@hotmail.com>
2018-03-04 16:40:35 +0200

    Updated Add entry page to redirect to the list

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -55,6 +55,7 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.AddEntry(entry);
                 // display index
+                return RedirectToAction("Index");
             }
             return View();
         }
